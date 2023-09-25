ec546dcb0faa9f07fdf66bfd682bd3dee35e40b8
Ben Green <bkingsleygreen@gmail.com>
2023-09-25 04:40:58 +0100

    Added output caching, added trailing slashes to endpoint urls, removed http block from launchsettings.json

--- src/EmbassyAirlines.Api/Apis/FleetApi.cs
+++ src/EmbassyAirlines.Api/Apis/FleetApi.cs
@@ -4,6 +4,7 @@ using EmbassyAirlines.Application.Exceptions;
 using EmbassyAirlines.Application.Queries;
 using Mediator;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 
 namespace EmbassyAirlines.Api.Apis;
 
@@ -11,28 +12,31 @@ public static class FleetApi
 {
     public static WebApplication MapFleetApi(this WebApplication app)
     {
-        app.MapGet("/api/fleet", async ([FromServices] IMediator mediator, CancellationToken cancellationToken) =>
+        app.MapGet("/api/fleet/", async ([FromServices] IMediator mediator, CancellationToken ct) =>
         {
-            var fleet = await mediator.Send(new GetFleet(), cancellationToken);
+            var fleet = await mediator.Send(new GetFleet(), ct);
             return Results.Ok(fleet);
-        }).WithName("Get fleet");
-        app.MapGet("/api/fleet/{id}", async ([FromRoute] Guid id, [FromServices] IMediator mediator, CancellationToken cancellationToken) =>
+        }).CacheOutput(x => x.Tag("fleet"))
+            .WithName("Get fleet");
+        app.MapGet("/api/fleet/{id:guid}/", async ([FromRoute] Guid id, [FromServices] IMediator mediator, CancellationToken ct) =>
         {
             try
             {
-                var aircraft = await mediator.Send(new GetAircraftById(id), cancellationToken);
+                var aircraft = await mediator.Send(new GetAircraftById(id), ct);
                 return Results.Ok(aircraft);
             }
             catch (NotFoundException ex)
             {
                 return Results.NotFound(ex.Message);
             }
-        }).WithName("Get aircraft by id");
-        app.MapPost("/api/fleet", async ([FromBody] NewAircraftDto dto, [FromServices] IMediator mediator, CancellationToken cancellationToken) =>
+        }).CacheOutput(x => x.AddPolicy<ByIdCachePolicy>())
+            .WithName("Get aircraft by id");
+        app.MapPost("/api/fleet/", async ([FromBody] NewAircraftDto dto, [FromServices] IMediator mediator, IOutputCacheStore cache, CancellationToken ct) =>
         {
             try
             {
-                var response = await mediator.Send(dto, cancellationToken);
+                var response = await mediator.Send(dto, ct);
+                await cache.EvictByTagAsync("fleet", ct);
                 return Results.Created($"/api/fleet/{response.Id}", response);
             }
             catch (ValidationException ex)
@@ -40,11 +44,12 @@ public static class FleetApi
                 return Results.BadRequest(ex.ValidationError);
             }
         }).WithName("Add aircraft");
-        app.MapPut("/api/fleet/{id}", async ([FromRoute] Guid id, [FromBody] UpdateAircraftDto dto, [FromServices] IMediator mediator, CancellationToken cancellationToken) =>
+        app.MapPut("/api/fleet/{id:guid}/", async ([FromRoute] Guid id, [FromBody] UpdateAircraftDto dto, [FromServices] IMediator mediator, IOutputCacheStore cache, CancellationToken ct) =>
         {
             try
             {
-                var response = await mediator.Send(new UpdateAircraft(id, dto), cancellationToken);
+                var response = await mediator.Send(new UpdateAircraft(id, dto), ct);
+                await cache.EvictByTagAsync(id.ToString(), ct);
                 return Results.Ok(response);
             }
             catch (NotFoundException ex)
@@ -56,11 +61,11 @@ public static class FleetApi
                 return Results.BadRequest(ex.ValidationError);
             }
         }).WithName("Update aircraft");
-        app.MapDelete("/api/fleet/{id}", async ([FromRoute] Guid id, [FromServices] IMediator mediator, CancellationToken cancellationToken) =>
+        app.MapDelete("/api/fleet/{id:guid}/", async ([FromRoute] Guid id, [FromServices] IMediator mediator, CancellationToken ct) =>
         {
             try
             {
-                await mediator.Send(new DeleteAircraft(id), cancellationToken);
+                await mediator.Send(new DeleteAircraft(id), ct);
                 return Results.NoContent();
             }
             catch (NotFoundException ex)
---
+++ src/EmbassyAirlines.Api/ByIdCachePolicy.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+using Microsoft.Extensions.Primitives;
+
+namespace EmbassyAirlines.Api;
+
+public class ByIdCachePolicy : IOutputCachePolicy
+{
+    ValueTask IOutputCachePolicy.CacheRequestAsync(OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        var idRouteVal = context.HttpContext.Request.RouteValues["id"];
+        if (idRouteVal is null)
+        {
+            return ValueTask.CompletedTask;
+        }
+        context.Tags.Add(idRouteVal.ToString()!);
+
+        var attemptOutputCaching = AttemptOutputCaching(context);
+        context.EnableOutputCaching = true;
+        context.AllowCacheLookup = attemptOutputCaching;
+        context.AllowCacheStorage = attemptOutputCaching;
+        context.AllowLocking = true;
+        context.CacheVaryByRules.QueryKeys = "*";
+
+        return ValueTask.CompletedTask;
+    }
+
+    ValueTask IOutputCachePolicy.ServeFromCacheAsync(OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    ValueTask IOutputCachePolicy.ServeResponseAsync(OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        var response = context.HttpContext.Response;
+
+        if (!StringValues.IsNullOrEmpty(response.Headers.SetCookie))
+        {
+            context.AllowCacheStorage = false;
+            return ValueTask.CompletedTask;
+        }
+
+        if (response.StatusCode != StatusCodes.Status200OK)
+        {
+            context.AllowCacheStorage = false;
+            return ValueTask.CompletedTask;
+        }
+
+        return ValueTask.CompletedTask;
+    }
+
+    private static bool AttemptOutputCaching(OutputCacheContext context)
+    {
+        var request = context.HttpContext.Request;
+
+        if (!HttpMethods.IsGet(request.Method) && !HttpMethods.IsHead(request.Method))
+        {
+            return false;
+        }
+
+        if (!StringValues.IsNullOrEmpty(request.Headers.Authorization) || request.HttpContext.User?.Identity?.IsAuthenticated == true)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
