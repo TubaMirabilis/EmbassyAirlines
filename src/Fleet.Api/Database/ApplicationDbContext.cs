e65110d028223bca6dd5568b938c4795fb312cdc
Benjamin Green <bkingsleygreen@gmail.com>
2024-03-29 07:47:14 +0000

    Started configuring DbContext

--- src/Fleet.Api/Database/ApplicationDbContext.cs
+++ src/Fleet.Api/Database/ApplicationDbContext.cs
@@ -10,5 +10,12 @@ public class ApplicationDbContext : DbContext
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(modelBuilder);
+        modelBuilder.Entity<Aircraft>().Property(a => a.Registration).HasMaxLength(12).HasColumnType("varchar(12)");
+        modelBuilder.Entity<Aircraft>().Property(a => a.Status).HasMaxLength(20).HasColumnType("varchar(20)");
+    }   
+
     public DbSet<Aircraft> Aircraft { get; set; }
 }
