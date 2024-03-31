831678d66ad46cbbe112a9817477e60a2058f0ad
Ben Green <bkingsleygreen@gmail.com>
2024-03-31 08:37:11 +0100

    Better logging

--- src/Fleet.Api/Features/Aircraft/GetAircraft.cs
+++ src/Fleet.Api/Features/Aircraft/GetAircraft.cs
@@ -17,12 +17,15 @@ public static class GetAircraft
     internal sealed class Handler : IRequestHandler<Query, ErrorOr<AircraftResponse>>
     {
         private readonly ApplicationDbContext _ctx;
-        public Handler(ApplicationDbContext ctx)
+        private readonly ILogger<Handler> _logger;
+        public Handler(ApplicationDbContext ctx, ILogger<Handler> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
         public async Task<ErrorOr<AircraftResponse>> Handle(Query request, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("Getting aircraft by id {id}", request.Id);
             var aircraftResponse = await _ctx.Aircraft
                                          .AsNoTracking()
                                          .Where(aircraft => aircraft.Id == request.Id)
@@ -31,18 +34,26 @@ public static class GetAircraft
                                          .FirstOrDefaultAsync(cancellationToken);
             if (aircraftResponse is null)
             {
+                _logger.LogWarning("Aircraft not found");
                 return Error.NotFound("Aircraft not found");
             }
+            _logger.LogInformation("Aircraft found");
             return aircraftResponse;
         }
     }
 }
 public class GetAircraftEndpoint : ICarterModule
 {
+    private readonly ILogger<GetAircraftEndpoint> _logger;
+    public GetAircraftEndpoint(ILogger<GetAircraftEndpoint> logger)
+    {
+        _logger = logger;
+    }
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapGet("api/aircraft/{id}", async (Guid id, ISender sender) =>
         {
+            _logger.LogInformation("Getting aircraft by id {id}", id);
             var query = new GetAircraft.Query { Id = id };
             var result = await sender.Send(query);
             return result.Match(
