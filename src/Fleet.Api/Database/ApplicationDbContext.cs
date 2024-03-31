8e59b24277cb0869f2177240a5d35165e09829d9
Ben Green <bkingsleygreen@gmail.com>
2024-03-31 08:21:47 +0100

    Inserted line breaks

--- src/Fleet.Api/Database/ApplicationDbContext.cs
+++ src/Fleet.Api/Database/ApplicationDbContext.cs
@@ -24,11 +24,11 @@ public class ApplicationDbContext : DbContext
             .HasMaxLength(20).HasColumnType("varchar(20)");
         modelBuilder.Entity<Aircraft>().Property(a => a.TypeDesignator).HasMaxLength(4).HasColumnType("varchar(4)");
         modelBuilder.Entity<Aircraft>().Property(a => a.EngineModel).HasMaxLength(50).HasColumnType("varchar(50)");
-        // SeatingConfiguration is a dictionary, so we need to configure it as a JSON column. ef core will handle serialization/deserialization
         modelBuilder.Entity<Aircraft>().Property(a => a.SeatingConfiguration).HasColumnType("json")
             .HasConversion(
                 v => JsonSerializer.Serialize<Dictionary<string, short>>(v, options),
-                v => JsonSerializer.Deserialize<Dictionary<string, short>>(v, options) ?? new Dictionary<string, short>()
+                v => JsonSerializer.Deserialize<Dictionary<string, short>>(v, options)
+                    ?? new Dictionary<string, short>()
             );
     }
 
