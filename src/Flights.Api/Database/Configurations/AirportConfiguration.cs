d4f0139d70ba1cef149d7b597aefe290dd359907
TubaMirabilis <bkingsleygreen@gmail.com>
2025-01-31 13:27:54 +0000

    Ran dotnet format

--- src/Flights.Api/Database/Configurations/AirportConfiguration.cs
+++ src/Flights.Api/Database/Configurations/AirportConfiguration.cs
@@ -6,21 +6,21 @@ namespace Flights.Api.Database.Configurations;
 
 internal sealed class AirportConfiguration : IEntityTypeConfiguration<Airport>
 {
-       public void Configure(EntityTypeBuilder<Airport> builder)
-       {
-              builder.HasKey(b => b.Id);
-              builder.Property(b => b.CreatedAt)
-                     .IsRequired();
-              builder.Property(b => b.UpdatedAt)
-                     .IsRequired();
-              builder.Property(b => b.Name)
-                     .IsRequired()
-                     .HasMaxLength(100);
-              builder.Property(b => b.TimeZoneId)
-                     .IsRequired()
-                     .HasMaxLength(100);
-              builder.Property(b => b.IataCode)
-                     .IsRequired()
-                     .HasMaxLength(3);
-       }
+    public void Configure(EntityTypeBuilder<Airport> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.CreatedAt)
+               .IsRequired();
+        builder.Property(b => b.UpdatedAt)
+               .IsRequired();
+        builder.Property(b => b.Name)
+               .IsRequired()
+               .HasMaxLength(100);
+        builder.Property(b => b.TimeZoneId)
+               .IsRequired()
+               .HasMaxLength(100);
+        builder.Property(b => b.IataCode)
+               .IsRequired()
+               .HasMaxLength(3);
+    }
 }
