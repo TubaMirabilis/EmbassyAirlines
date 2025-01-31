09246039847f06ac2b4ec340123311384c7f1d27
TubaMirabilis <bkingsleygreen@gmail.com>
2025-01-31 03:20:37 +0000

    Ran dotnet format

--- src/Flights.Api/Database/Configurations/BookingConfiguration.cs
+++ src/Flights.Api/Database/Configurations/BookingConfiguration.cs
@@ -6,17 +6,17 @@ namespace Flights.Api.Database.Configurations;
 
 internal sealed class BookingConfiguration : IEntityTypeConfiguration<Booking>
 {
-       public void Configure(EntityTypeBuilder<Booking> builder)
-       {
-              builder.HasKey(b => b.Id);
-              builder.Property(b => b.CreatedAt)
-                     .IsRequired();
-              builder.Property(b => b.UpdatedAt)
-                     .IsRequired();
-              builder.HasMany(b => b.Passengers)
-                     .WithOne()
-                     .HasForeignKey(p => p.BookingId)
-                     .IsRequired()
-                     .OnDelete(DeleteBehavior.Cascade);
-       }
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.HasKey(b => b.Id);
+        builder.Property(b => b.CreatedAt)
+               .IsRequired();
+        builder.Property(b => b.UpdatedAt)
+               .IsRequired();
+        builder.HasMany(b => b.Passengers)
+               .WithOne()
+               .HasForeignKey(p => p.BookingId)
+               .IsRequired()
+               .OnDelete(DeleteBehavior.Cascade);
+    }
 }
