3d2a5ce8a255e7cd01377630b1c10574619a2030
TubaMirabilis <bkingsleygreen@gmail.com>
2025-02-04 05:47:25 +0000

    New test project

---
+++ tests/Flights.Api.FunctionalTests/Abstractions/BaseFunctionalTest.cs
@@ -0,0 +1,7 @@
+﻿namespace Flights.Api.FunctionalTests.Abstractions;
+
+public abstract class BaseFunctionalTest : IClassFixture<FunctionalTestWebAppFactory>
+{
+    protected BaseFunctionalTest(FunctionalTestWebAppFactory factory) => HttpClient = factory.CreateClient();
+    protected HttpClient HttpClient { get; set; }
+}
---
+++ tests/Flights.Api.FunctionalTests/Abstractions/FunctionalTestWebAppFactory.cs
@@ -0,0 +1,34 @@
+﻿using Flights.Api.Database;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Testcontainers.PostgreSql;
+
+namespace Flights.Api.FunctionalTests.Abstractions;
+
+public class FunctionalTestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("runtrackr")
+        .WithUsername("postgres")
+        .WithPassword("postgres")
+        .Build();
+    protected override void ConfigureWebHost(IWebHostBuilder builder) => builder.ConfigureTestServices(services =>
+                                                                              {
+                                                                                  services.RemoveAll<DbContextOptions<ApplicationDbContext>>();
+                                                                                  services.AddDbContext<ApplicationDbContext>(options =>
+                                                                                      options
+                                                                                          .UseNpgsql(_dbContainer.GetConnectionString())
+                                                                                          .UseSnakeCaseNamingConvention());
+                                                                              });
+    public async Task InitializeAsync() => await _dbContainer.StartAsync();
+    public new async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+        await _dbContainer.DisposeAsync();
+    }
+}
