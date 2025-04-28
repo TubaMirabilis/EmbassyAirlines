5a7bfe009ce016f33f7b0849216cb2f85913d75d
TubaMirabilis <bkingsleygreen@gmail.com>
2025-04-28 16:17:45 +0100

    Updated dependencies

---
+++ src/Aircraft.Api.Lambda/CreateOrUpdateAircraftDtoValidator.cs
@@ -0,0 +1,35 @@
+using FluentValidation;
+
+namespace Aircraft.Api.Lambda;
+
+public sealed class CreateOrUpdateAircraftDtoValidator : AbstractValidator<CreateOrUpdateAircraftDto>
+{
+    public CreateOrUpdateAircraftDtoValidator()
+    {
+        RuleFor(x => x.TailNumber)
+            .Matches(@"^[A-Z0-9\-]+$")
+            .WithMessage("Tail number must only contain uppercase letters, numbers, and dashes.")
+            .MaximumLength(12)
+            .WithMessage("Tail number must be no longer than 12 characters.");
+        RuleFor(x => x.EquipmentCode)
+            .Matches(@"^[A-Z0-9]+$")
+            .WithMessage("Equipment code must only contain uppercase letters and numbers.")
+            .MaximumLength(4)
+            .WithMessage("Equipment code must be no longer than 4 characters.");
+        RuleFor(x => x.DryOperatingWeight)
+                    .GreaterThan(0)
+                    .WithMessage("Dry operating weight must be greater than zero.");
+        RuleFor(x => x.MaximumTakeoffWeight)
+            .GreaterThan(0)
+            .WithMessage("Maximum takeoff weight must be greater than zero.");
+        RuleFor(x => x.MaximumLandingWeight)
+            .GreaterThan(0)
+            .WithMessage("Maximum landing weight must be greater than zero.");
+        RuleFor(x => x.MaximumZeroFuelWeight)
+            .GreaterThan(0)
+            .WithMessage("Maximum zero fuel weight must be greater than zero.");
+        RuleFor(x => x.MaximumFuelWeight)
+            .GreaterThan(0)
+            .WithMessage("Maximum fuel weight must be greater than zero.");
+    }
+}
