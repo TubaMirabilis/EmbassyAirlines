e32195f995fe82b7b5141f0b4c54380acaceb9a6
a11yChief <richard.hyman@welcomeally.com>
2025-08-18 20:39:54 +0100

    Renamed repo to embassy-web

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -1,6 +1,6 @@
 module "web" {
   source                 = "github.com/welcome-ally-ltd/aws-ecr-repo"
-  repo_name              = "example"
+  repo_name              = "embassy-web"
   force_delete           = true
   enable_lambda_function = false
 
