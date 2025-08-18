982b066a9f898a3bc4b9d2c55216e33fc7979929
a11yChief <richard.hyman@welcomeally.com>
2025-08-18 19:48:37 +0100

    Ran terraform fmt

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -1,7 +1,7 @@
 module "ecr_repo" {
-    source = "github.com/welcome-ally-ltd/aws-ecr-repo"
-    repo_name = "example"
-    force_delete = true 
-    enable_lambda_function = false
-    
+  source                 = "github.com/welcome-ally-ltd/aws-ecr-repo"
+  repo_name              = "example"
+  force_delete           = true
+  enable_lambda_function = false
+
 }
