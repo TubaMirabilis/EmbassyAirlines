89dd8c3f4eb3c3022c3c993a5a770ec7bd6549d6
a11yChief <richard.hyman@welcomeally.com>
2025-08-18 20:05:29 +0100

    Enable lambda function for ecr repos

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -2,6 +2,6 @@ module "ecr_repo" {
   source                 = "github.com/welcome-ally-ltd/aws-ecr-repo"
   repo_name              = "example"
   force_delete           = true
-  enable_lambda_function = false
+  enable_lambda_function = true
 
 }
