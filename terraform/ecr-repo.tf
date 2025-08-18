4ea6fb063459c99d1854c33e1c24f8fdd5ae9c16
a11yChief <richard.hyman@welcomeally.com>
2025-08-18 19:41:50 +0100

    Renamed ecr repo

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -1,6 +1,6 @@
-module "ecr_set" {
+module "ecr_repo" {
     source = "github.com/welcome-ally-ltd/aws-ecr-repo"
-    repo_name = "test"
+    repo_name = "example"
     force_delete = true 
     enable_lambda_function = false
     
