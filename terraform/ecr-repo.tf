014cf404e35aecd47364d69afe3632ac8a346fdb
a11yChief <richard.hyman@welcomeally.com>
2025-08-18 20:49:11 +0100

    Reenabling lambda functions

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -2,6 +2,6 @@ module "web" {
   source                 = "github.com/welcome-ally-ltd/aws-ecr-repo"
   repo_name              = "embassy-web"
   force_delete           = true
-  enable_lambda_function = false
+  enable_lambda_function = true
 
 }
