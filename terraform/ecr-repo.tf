a3f300333abdfea6207c3cdd4c13ac0687638d26
a11yChief <richard.hyman@welcomeally.com>
2025-08-18 18:18:18 +0100

    Updated source url in ecr terraform

--- terraform/ecr-repo.tf
+++ terraform/ecr-repo.tf
@@ -1,5 +1,5 @@
 module "ecr_set" {
-    source = "https://github.com/welcome-ally-ltd/aws-ecr-repo"
+    source = "github.com/welcome-ally-ltd/aws-ecr-repo"
     repo_name = "test"
     force_delete = true 
     enable_lambda_function = false
