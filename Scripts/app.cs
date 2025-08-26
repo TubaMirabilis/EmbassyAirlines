24364223df310b5eeb2bf8ceae20576028ddaaed
TubaMirabilis <bkingsleygreen@gmail.com>
2025-08-26 08:34:38 +0100

    Minor changes

--- Scripts/app.cs
+++ Scripts/app.cs
@@ -1,6 +1,5 @@
 ﻿#:package AWSSDK.ECR@4.0.3.6
 #:package Ductus.FluentDocker@2.85.0
-
 using Amazon.ECR;
 using Amazon.ECR.Model;
 using Ductus.FluentDocker.Builders;
@@ -11,5 +10,16 @@ var res = await client.DescribeRegistryAsync(req);
 Console.WriteLine($"Registry ID: {res.RegistryId}");
 Console.WriteLine("Building Docker image...");
 var dir = Directory.GetCurrentDirectory();
-var imageService = new Builder().DefineImage("tubamirabilis/example").ReuseIfAlreadyExists().FromFile($"{dir}/docker/Example.Api.Lambda.dockerfile").WorkingFolder(dir).Build();
-Console.WriteLine($"Image ID: {imageService.Id}");
+var dockerfilePath = Path.Combine(dir, "docker", "Example.Api.Lambda.dockerfile");
+if (!File.Exists(dockerfilePath))
+{
+    Console.WriteLine($"Dockerfile not found at path: {dockerfilePath}");
+    return;
+}
+var dockerfileContents = await File.ReadAllTextAsync(dockerfilePath);
+var image = new Builder().DefineImage("tubamirabilis/example")
+                         .ReuseIfAlreadyExists()
+                         .FromString(dockerfileContents)
+                         .WorkingFolder(dir)
+                         .Build();
+Console.WriteLine($"Image ID: {image.Id}");
