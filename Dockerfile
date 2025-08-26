b9e4f662621a321448a6ce431a081e6163b8d5e0
TubaMirabilis <bkingsleygreen@gmail.com>
2025-08-26 08:11:02 +0100

    Experimenting with Ductus.FluentDocker in app.cs

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM mcr.microsoft.com/dotnet/sdk:10.0-preview AS build
+WORKDIR /src
+COPY ./src/Example.Api.Lambda/ /src/Example.Api.Lambda/
+COPY ./src/Shared/ /src/Shared/
+WORKDIR /src/Example.Api.Lambda
+RUN dotnet restore
+RUN dotnet publish -c Release --os linux -o /app/publish
+
+FROM public.ecr.aws/lambda/dotnet:9
+WORKDIR /var/task
+COPY --from=build /app/publish .
+CMD ["Example.Api.Lambda"]
--- Scripts/app.cs
+++ Scripts/app.cs
@@ -1,9 +1,15 @@
 ﻿#:package AWSSDK.ECR@4.0.3.6
+#:package Ductus.FluentDocker@2.85.0
 
 using Amazon.ECR;
 using Amazon.ECR.Model;
+using Ductus.FluentDocker.Builders;
 
 using var client = new AmazonECRClient();
 var req = new DescribeRegistryRequest();
 var res = await client.DescribeRegistryAsync(req);
-Console.WriteLine(res.RegistryId);
+Console.WriteLine($"Registry ID: {res.RegistryId}");
+Console.WriteLine("Building Docker image...");
+var dir = Directory.GetCurrentDirectory();
+var imageService = new Builder().DefineImage("tubamirabilis/example").ReuseIfAlreadyExists().FromFile($"{dir}/docker/Example.Api.Lambda.dockerfile").WorkingFolder(dir).Build();
+Console.WriteLine($"Image ID: {imageService.Id}");
