92aa25e1b7f0461864b4d50ec99e9e32591bcca2
TubaMirabilis <bkingsleygreen@gmail.com>
2025-08-30 11:28:41 +0100

    Removed terraform files

--- terraform/ecr-repo.tf
+++
@@ -1,7 +0,0 @@
-module "web" {
-  source                 = "github.com/welcome-ally-ltd/aws-ecr-repo"
-  repo_name              = "embassy-web"
-  force_delete           = true
-  enable_lambda_function = true
-
-}
