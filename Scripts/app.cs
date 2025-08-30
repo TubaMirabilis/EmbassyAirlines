60da6c039ab0f48dcde4f63be07ba18279e11102
TubaMirabilis <bkingsleygreen@gmail.com>
2025-08-30 11:24:55 +0100

    Updated dependencies

--- Scripts/app.cs
+++ Scripts/app.cs
@@ -1,25 +1,10 @@
-﻿#:package AWSSDK.ECR@4.0.3.6
+﻿#:package AWSSDK.ECR@4.0.4.1
+#:package AWSSDK.EC2@4.0.35
 #:package Ductus.FluentDocker@2.85.0
-using Amazon.ECR;
-using Amazon.ECR.Model;
-using Ductus.FluentDocker.Builders;
 
-using var client = new AmazonECRClient();
-var req = new DescribeRegistryRequest();
-var res = await client.DescribeRegistryAsync(req);
-Console.WriteLine($"Registry ID: {res.RegistryId}");
-Console.WriteLine("Building Docker image...");
-var dir = Directory.GetCurrentDirectory();
-var dockerfilePath = Path.Combine(dir, "docker", "Example.Api.Lambda.dockerfile");
-if (!File.Exists(dockerfilePath))
-{
-    Console.WriteLine($"Dockerfile not found at path: {dockerfilePath}");
-    return;
-}
-var dockerfileContents = await File.ReadAllTextAsync(dockerfilePath);
-var image = new Builder().DefineImage("tubamirabilis/example")
-                         .ReuseIfAlreadyExists()
-                         .FromString(dockerfileContents)
-                         .WorkingFolder(dir)
-                         .Build();
-Console.WriteLine($"Image ID: {image.Id}");
+using Amazon.EC2;
+using Amazon.EC2.Model;
+
+using var client = new AmazonEC2Client();
+var req = new CreateVpcRequest();
+var res = await client.CreateVpcAsync(req);
