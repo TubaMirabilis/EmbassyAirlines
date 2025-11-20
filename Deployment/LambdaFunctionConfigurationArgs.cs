5fc8ebe4b5f6fbc7f0402622f110d54e1b6050e2
TubaMirabilis <bkingsleygreen@gmail.com>
2025-11-20 12:23:20 +0000

    Switched to AWS CDK

--- Deployment/LambdaFunctionConfigurationArgs.cs
+++
@@ -1,9 +0,0 @@
-namespace Deployment;
-
-internal sealed record LambdaFunctionConfigurationArgs
-{
-    public required Dictionary<string, string> Environment { get; init; }
-    public required string FunctionName { get; init; }
-    public required string ImageUri { get; init; }
-    public required string RoleArn { get; init; }
-}
