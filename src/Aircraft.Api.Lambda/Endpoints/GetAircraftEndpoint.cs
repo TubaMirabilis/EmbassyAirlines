e98e3212a1870e52ee8165365ff64af60a8407a7
TubaMirabilis <bkingsleygreen@gmail.com>
2025-12-01 05:33:14 +0000

    Enhanced GetAircraftEndpoint

--- src/Aircraft.Api.Lambda/Endpoints/GetAircraftEndpoint.cs
+++ src/Aircraft.Api.Lambda/Endpoints/GetAircraftEndpoint.cs
@@ -8,18 +8,19 @@ namespace Aircraft.Api.Lambda.Endpoints;
 
 internal sealed class GetAircraftEndpoint : IEndpoint
 {
-    private readonly ILogger<GetAircraftEndpoint> _logger;
-    public GetAircraftEndpoint(ILogger<GetAircraftEndpoint> logger) => _logger = logger;
     public void MapEndpoint(IEndpointRouteBuilder app)
         => app.MapGet("aircraft/{id}", InvokeAsync);
-    private async Task<IResult> InvokeAsync(ApplicationDbContext ctx, Guid id, CancellationToken ct)
+    private static async Task<IResult> InvokeAsync(ApplicationDbContext ctx,
+                                                   ILogger<GetAircraftEndpoint> logger,
+                                                   Guid id,
+                                                   CancellationToken ct)
     {
         var aircraft = await ctx.Aircraft
                                  .Include(a => a.Seats)
                                  .FirstOrDefaultAsync(a => a.Id == id, ct);
         if (aircraft is null)
         {
-            _logger.LogWarning("Aircraft with ID {Id} not found", id);
+            logger.LogWarning("Aircraft with ID {Id} not found", id);
             var error = Error.NotFound("Aircraft.NotFound", $"Aircraft with ID {id} not found");
             return ErrorHandlingHelper.HandleProblem(error);
         }
