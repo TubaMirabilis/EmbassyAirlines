1469f56ea4a2ffc8796e264bb21f6fd0de2d99a5
TubaMirabilis <bkingsleygreen@gmail.com>
2026-01-10 19:08:00 +0000

    Removed extraneous interceptor from Aircraft.Infrastructure project

--- src/Aircraft.Infrastructure/ServiceCollectionExtensions.cs
+++ src/Aircraft.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,8 +1,11 @@
+using System.Globalization;
 using Aircraft.Infrastructure.Database;
+using Amazon.RDS.Util;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
+using Shared;
 
 namespace Aircraft.Infrastructure;
 
@@ -12,19 +15,33 @@ public static class ServiceCollectionExtensions
     {
         var host = config["DbConnection:Host"];
         var dbName = config["DbConnection:Database"];
+        var username = config["DbConnection:Username"];
+        var portStr = config["DbConnection:Port"];
+        Ensure.NotNullOrEmpty(host);
+        Ensure.NotNullOrEmpty(dbName);
+        Ensure.NotNullOrEmpty(username);
+        Ensure.NotNullOrEmpty(portStr);
+        var port = int.Parse(portStr, CultureInfo.InvariantCulture);
         var connectionString = new NpgsqlConnectionStringBuilder
         {
+            Database = dbName,
             Host = host,
-            Database = dbName
+            Port = port,
+            SslMode = SslMode.Require,
+            Username = username
         }.ConnectionString;
-        services.AddSingleton<EntityFrameworkInterceptor>();
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        dataSourceBuilder.UsePasswordProvider(
+            passwordProvider: _ => throw new NotSupportedException("Use OpenAsync"),
+            passwordProviderAsync: async (builder, ct) => await RDSAuthTokenGenerator.GenerateAuthTokenAsync(host, port, username));
+        var dataSource = dataSourceBuilder.Build();
+        services.AddSingleton(dataSource);
         services.AddDbContext<ApplicationDbContext>((sp, options) => options.UseNpgsql(new NpgsqlConnection(connectionString), x =>
         {
             x.MigrationsHistoryTable("__EFMigrationsHistory", "aircraft");
             x.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
         })
-        .UseSnakeCaseNamingConvention()
-        .AddInterceptors(sp.GetRequiredService<EntityFrameworkInterceptor>()));
+        .UseSnakeCaseNamingConvention());
         return services;
     }
 }
