2e24fae1fbb348a4d0f46c5e2cd6865b2d7f4b48
TubaMirabilis <bkingsleygreen@gmail.com>
2026-06-15 11:31:35 +0100

    Better .NET Aspire AppHost

--- src/AWS.Aspire.AppHost/AppHost.cs
+++ src/AWS.Aspire.AppHost/AppHost.cs
@@ -1,28 +1,43 @@
 using Amazon;
-using Amazon.CDK.AWS.DynamoDB;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Aspire.Hosting.AWS.Lambda;
 
 var builder = DistributedApplication.CreateBuilder(args);
 var awsSdkConfig = builder.AddAWSSDKConfig().WithRegion(RegionEndpoint.EUWest2);
 var awsResources = builder.AddAWSCDKStack("AirportsServiceAWSResources").WithReference(awsSdkConfig);
-builder.AddAWSDynamoDBLocal("DynamoDBAirports").WithReference(awsSdkConfig);
-var airportsTable = awsResources.AddDynamoDBTable("AirportsTable", new TableProps
+var dynamoDb = builder.AddAWSDynamoDBLocal("DynamoDBAirports");
+builder.Eventing.Subscribe<ResourceReadyEvent>(dynamoDb.Resource, async (evnt, ct) =>
 {
-    TableName = "airports",
-    PartitionKey = new Amazon.CDK.AWS.DynamoDB.Attribute
+    var serviceUrl = dynamoDb.Resource.GetEndpoint("http").Url;
+    var ddbClient = new AmazonDynamoDBClient(new AmazonDynamoDBConfig { ServiceURL = serviceUrl });
+    await ddbClient.CreateTableAsync(new CreateTableRequest
     {
-        Name = "Id",
-        Type = AttributeType.STRING
-    }
+        TableName = "airports",
+        KeySchema =
+            [
+                new KeySchemaElement("Id", KeyType.HASH)
+            ],
+        AttributeDefinitions =
+            [
+                new AttributeDefinition("Id", ScalarAttributeType.S)
+            ],
+        ProvisionedThroughput = new ProvisionedThroughput(1, 1)
+    }, ct);
 });
-var topic1 = awsResources.AddSNSTopic("AirportCreatedTopic");
-var topic2 = awsResources.AddSNSTopic("AirportUpdatedTopic");
+var topic1 = awsResources.AddSNSTopic("AirportCreatedTopic").AddOutput("AirportCreatedTopicArn", t => t.TopicArn);
+var topic2 = awsResources.AddSNSTopic("AirportUpdatedTopic").AddOutput("AirportUpdatedTopicArn", t => t.TopicArn);
 builder.AddAWSLambdaServiceEmulator();
 var airportsApiLambda = builder.AddAWSLambdaFunction<Projects.Airports_Api_Lambda>("airports-api", "Airports.Api.Lambda")
+                               .WaitFor(dynamoDb)
+                               .WaitFor(topic1)
+                               .WaitFor(topic2)
                                .WithReference(awsSdkConfig)
+                               .WithReference(dynamoDb)
                                .WithReference(topic1)
                                .WithReference(topic2)
-                               .WithReference(airportsTable)
+                               .WithEnvironment("AIRPORTS_SNS__AirportCreatedTopicArn", topic1, t => t.TopicArn, "AirportCreatedTopicArn")
+                               .WithEnvironment("AIRPORTS_SNS__AirportUpdatedTopicArn", topic2, t => t.TopicArn, "AirportUpdatedTopicArn")
                                .WithEnvironment("AIRPORTS_DynamoDb__TableName", "airports");
 builder.AddAWSAPIGatewayEmulator("api-gateway", APIGatewayType.HttpV2, new APIGatewayEmulatorOptions
 {
