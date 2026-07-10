de348ac96dcdff426403d0e0831d90843bdd449e
TubaMirabilis <bkingsleygreen@gmail.com>
2026-07-10 20:39:22 +0100

    New Shared.AWS.CloudWatchLogs class library project

--- scripts/GetAircraftLogs.cs
+++ scripts/GetAircraftLogs.cs
@@ -1,27 +1,6 @@
-#:package AWSSDK.CloudWatchLogs
-using Amazon.CloudWatchLogs;
+#:project ../src/Shared.AWS.CloudWatchLogs
+using Shared.AWS.CloudWatchLogs;
 
-using var client = new AmazonCloudWatchLogsClient();
-const int limit = 50;
 var logGroupName = "/aws/lambda/AircraftApiLambda";
-
-var logStreamsResponse = await client.DescribeLogStreamsAsync(new Amazon.CloudWatchLogs.Model.DescribeLogStreamsRequest
-{
-    LogGroupName = logGroupName,
-    OrderBy = "LastEventTime",
-    Descending = true,
-    Limit = 1
-});
-
-var logStreamName = logStreamsResponse.LogStreams[0].LogStreamName;
-
-var logEventsResponse = await client.GetLogEventsAsync(new Amazon.CloudWatchLogs.Model.GetLogEventsRequest
-{
-    LogGroupName = logGroupName,
-    LogStreamName = logStreamName,
-    Limit = limit,
-    StartFromHead = false
-});
-
-var logs = string.Join(Environment.NewLine, logEventsResponse.Events.Select(e => e.Message));
+var logs = CloudWatchLogsService.FetchRecentLogMessagesAsync(logGroupName);
 Console.WriteLine(logs);
---
+++ src/Shared.AWS.CloudWatchLogs/CloudWatchLogsService.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.CloudWatchLogs;
+using Amazon.CloudWatchLogs.Model;
+
+namespace Shared.AWS.CloudWatchLogs;
+
+public static class CloudWatchLogsService
+{
+    public static async Task<string> FetchRecentLogMessagesAsync(string logGroupName)
+    {
+        using var client = new AmazonCloudWatchLogsClient();
+        var logStreamsResponse = await client.DescribeLogStreamsAsync(new DescribeLogStreamsRequest
+        {
+            LogGroupName = logGroupName,
+            OrderBy = "LastEventTime",
+            Descending = true,
+            Limit = 1
+        });
+        var logStreamName = logStreamsResponse.LogStreams[0].LogStreamName;
+        var logEventsResponse = await client.GetLogEventsAsync(new GetLogEventsRequest
+        {
+            LogGroupName = logGroupName,
+            LogStreamName = logStreamName,
+            StartFromHead = false
+        });
+        return string.Join(Environment.NewLine, logEventsResponse.Events.Select(e => e.Message));
+    }
+}
