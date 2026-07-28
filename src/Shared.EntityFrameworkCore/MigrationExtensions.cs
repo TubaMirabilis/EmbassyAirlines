3f9b3b31ed33884ff6dab4af4d15047c47bf709e
TubaMirabilis <bkingsleygreen@gmail.com>
2026-07-28 09:59:03 +0100

    Added stopwatch for database migrations

--- src/Shared.EntityFrameworkCore/MigrationExtensions.cs
+++ src/Shared.EntityFrameworkCore/MigrationExtensions.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Shared.EntityFrameworkCore;
 
@@ -9,7 +11,11 @@ public static class MigrationExtensions
     public static async Task ApplyMigrationsAsync<TDbContext>(this IApplicationBuilder app) where TDbContext : DbContext
     {
         using var scope = app.ApplicationServices.CreateScope();
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<TDbContext>>();
         await using var dbContext = scope.ServiceProvider.GetRequiredService<TDbContext>();
+        var startTime = Stopwatch.GetTimestamp();
         await dbContext.Database.MigrateAsync();
+        var diff = Stopwatch.GetElapsedTime(startTime);
+        logger.LogInformation("Applied database migrations for {DbContext} in {ElapsedMilliseconds} ms.", typeof(TDbContext).Name, diff.TotalMilliseconds);
     }
 }
